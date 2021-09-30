2bd84bbbd398ab070e665466b560d951d776d37f
Marco Alberti <lbrmrc@unife.it>
2021-09-30 09:15:17 +0200

    esempi focus group 29 settembre 2021

---
+++ codice/focusgroup-2021-09-29/operatori.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* input intero */
+main() {
+    int a;
+    scanf("%d", &a);
+    ++a;
+    printf("%d\n", a); 
+}
+/* output è il triplo dell'input */
---
+++ codice/focusgroup-2021-09-29/variabili.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* input intero */
+main() {
+    int a;
+    int b;
+    scanf("%d", &a);
+    b = 3 * a;
+    printf("%d\n", b); 
+}
+/* output è il triplo dell'input */
