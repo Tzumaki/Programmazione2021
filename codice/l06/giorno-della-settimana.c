ed7250ef7f74bb07eb68a4b9e855cadf531e2b99
Marco Alberti <malbertife@gmail.com>
2021-10-28 13:12:06 +0200

    giorno della settimana

--- codice/l06/giorno-della-settimana.c
+++ codice/l06/giorno-della-settimana.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int giorno_giuliano(int g, int m, int a) {
   int N0, N1, N2, N3;
   N0 = (m - 14) / 12;
@@ -5,4 +7,44 @@ int giorno_giuliano(int g, int m, int a) {
   N2 = 367 * (m - 2 - 12 * N0) / 12;
   N3 = 3 * (a + 4900 + N0) / 400;
   return N1 + N2 - N3 + g - 32075;
+}
+
+char giorno(int giorno, int mese, int anno) {
+  // giorno giuliano della data <giorno,mese,anno>
+
+  switch (giorno_giuliano(giorno, mese, anno) % 7) {
+    // se 0 restituire 'l'
+    case 0:
+      return 'l';
+    case 1:
+      return 'm';
+    case 2:
+      return 'M';
+    case 3:
+      return 'g';
+    case 4:
+      return 'v';
+    case 5:
+      return 's';
+    case 6:
+      return 'd';
+  }
+}
+
+int ammissibile(int g, int m, int a) {
+  // dovrebbe verificare se la data è ammissibile, ad esempio 30/2/2021 non lo è
+  return 1;  // TODO
+}
+
+int main() {
+  int G, M, A;
+  printf("Digita giorno, mese e anno di una data\n");
+  scanf("%d%d%d", &G, &M, &A);
+  if (!ammissibile(G, M, A)) {
+    printf("Data non ammissibile\n");
+    return -1;
+  }
+  // stampa del giorno della settimana corrispondente
+  printf("%c\n", giorno(G, M, A));
+  return 0;
 }
