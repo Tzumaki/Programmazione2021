5fc033f712f4a62adaf7e973a8570be7cff9aa73
Marco Alberti <malbertife@gmail.com>
2021-10-28 12:22:15 +0200

    radice quadrata

--- codice/l06/radice-quadrata.c
+++ codice/l06/radice-quadrata.c
@@ -1,17 +1,26 @@
 #include <math.h>
 #include <stdio.h>
 
+float media(float a, float b) {
+  return (a + b) / 2.0;
+}
+
+float radq(float c) {
+  float x = 1.0;
+  while (fabsf(x * x - c) > 1e-5)
+    // assegno a x la media di x e c/x
+    x = media(x, c / x);
+  return x;
+}
+
 int main(void) {
-  float a, x;
+  float a;
   printf("Digita un numero\n");
   scanf("%f", &a);
   if (a < 0) {
     printf("%f e` negativo\n", a);
     return -1;
   }
-  x = 1.0;
-  while (fabsf(x * x - a) > 1e-5)
-    x = (x + a / x) / 2;
-  printf("La radice quadrata di %.2f e` circa %f\n", a, x);
+  printf("La radice quadrata di %.2f e` circa %f\n", a, radq(a));
   return 0;
 }
