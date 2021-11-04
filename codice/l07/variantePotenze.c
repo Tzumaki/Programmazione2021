fc229f4f916abcd1f7a599c77602da95ca39a1d8
Marco Alberti <malbertife@gmail.com>
2021-11-04 11:18:13 +0100

    variante potenze

--- codice/l07/variantePotenze.c
+++ codice/l07/variantePotenze.c
@@ -1,26 +1,38 @@
-#include<stdio.h>
+#include <stdio.h>
 // stampa a video le potenze negative sotto forma di frazione
-int potenza (int base, int esp){
-    int cont, prod = 1;
-    if(esp<0){
-        printf("1\n-\n");
-        return(potenza(base, -esp));
-    }
-    
-    for (cont = 0; cont < esp; cont++)
-        prod *= base;
+int potenza(int base, int esp) {
+  int cont, prod = 1;
 
-    return(prod);
+  for (cont = 0; cont < esp; cont++)
+    prod *= base;
+
+  return (prod);
 }
 
-int main(){
-    int b, e;
-    printf("-- CALCOLO POTENZE --\n");
-    printf("Inserisci base: ");
-    scanf("%d",&b);
-    printf("Inserisci esponente: ");
-    scanf("%d",&e);
-    printf("%d\n", potenza(b,e));
+int main() {
+  int b, e;
+  printf("-- CALCOLO POTENZE --\n");
+  printf("Inserisci base: ");
+  scanf("%d", &b);
+  printf("Inserisci esponente: ");
+  scanf("%d", &e);
 
-    return(0);
+  if (e < 0) {
+    printf("1\n-\n");
+    printf("%d\n", potenza(b, -e));
+  } else
+    printf("%d\n", potenza(b, e));
+
+  return (0);
 }
+
+/* Modificare l'output in modo che per
+
+b = 2, e = -20
+
+il programma stampi
+   1
+-------
+1048576
+
+*/
