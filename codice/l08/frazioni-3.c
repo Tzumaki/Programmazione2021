2908606f3e1a4dc1392235d521a41e5ade0e8fe7
Marco Alberti <malbertife@gmail.com>
2021-11-18 21:46:11 +0100

    frazioni v.2 e v.3

--- codice/l08/frazioni-3.c
+++ codice/l08/frazioni-3.c
@@ -50,7 +50,9 @@ void stampaDecimale(Frazione f) {
 int main() {
   Frazione f1, f2;
 
-  f1 = frazione(4, 6);
+  f1 = frazione(8, 2);
+  stampaFrazionaria(f1);
+  printf("\n");
   f2 = frazione(1, 5);
   stampaFrazionaria(somma(f1, f2));
   printf("\n");
