7c1c6b7416447a1c371445cea53f6a8824b84907
Marco Alberti <malbertife@gmail.com>
2022-01-12 13:09:07 +0100

    23 luglio 2021 con parte 2

--- codice/l15-simulazione-esame/20210723-con-parte-2/listaPersone.c
+++ codice/l15-simulazione-esame/20210723-con-parte-2/listaPersone.c
@@ -15,16 +15,34 @@ void insTesta(Lista* pl, Dato d) {
   *pl = aux;
 }
 
-void aggiorna(Lista* pl, Record r) {
+int giorno_giuliano(int g, int m, int a) {
+  // restituisce il numero di giorni trascorsi dal 1/1/4713 a.C alla data g/m/a
+  int N0, N1, N2, N3;
+  N0 = (m - 14) / 12;
+  N1 = 1461 * (a + 4800 + N0) / 4;
+  N2 = 367 * (m - 2 - 12 * N0) / 12;
+  N3 = 3 * (a + 4900 + N0) / 400;
+  return N1 + N2 - N3 + g - 32075;
+}
+
+int differenzaDate(Data d1, Data d2) {
+  return giorno_giuliano(d1.giorno, d1.mese, d1.anno) -
+         giorno_giuliano(d2.giorno, d2.mese, d2.anno);
+}
+
+void aggiorna(Lista* pl, Record r, Data oggi) {
+  // se evento troppo vecchio non faccio nullo
+  Data dataEvento = {r.giorno, r.mese, r.anno};
+  if (r.evento == 'T' && differenzaDate(oggi, dataEvento) > 3)
+    return;
+  if (r.evento == 'V' && differenzaDate(oggi, dataEvento) > 180)
+    return;
+
   // se CF già presente aggiorna il flag
   // altrimenti inserisce ordinatamente nuovo elemento
-
-  // salto tutti gli elementi con CF che precede alfabeticamente quello del record
   while (*pl != NULL && strcmp((*pl)->dato.CF, r.CF) < 0)
     pl = &(*pl)->next;
 
-
-  // se non ho trovato il CF creo un nuovo elemento
   if (*pl == NULL || strcmp((*pl)->dato.CF, r.CF) != 0) {
     Dato d;
     strcpy(d.CF, r.CF);
@@ -32,8 +50,6 @@ void aggiorna(Lista* pl, Record r) {
     d.vaccino = 0;
     insTesta(pl, d);
   }
-
-  // aggiorno i flag
   if (r.evento == 'V')
     (*pl)->dato.vaccino = 1;
   else if (r.evento == 'T')
